b2268554d811e8d9148fdcbc1953be7527bbeebc
stefano-franco <stefano.franco@nuvibit.com>
2023-03-08 14:41:02 +0100

    feat: adding two options for the parameters map

--- examples/complete/core-parameters.tf
+++ examples/complete/core-parameters.tf
@@ -2,9 +2,12 @@
 # ¦ CORE PARAMETERS MAP
 # ---------------------------------------------------------------------------------------------------------------------
 locals {
-  foundation_parameters = {
+
+  # for this option, the core-parameters module would need to add an additional level to the parameter store mapping
+  foundation_parameters_1 = {
     management : {
       main_region : "eu-central-1"
+      active_regions : ["eu-central-1", "eu-central-2"]
       read_tags_role_name : "read-account-tags-role"
       account_access_role : "OrganizationAccountAccessRole"
       organization : {
@@ -16,11 +19,17 @@ locals {
           "Workloads/Prod",
           "Workloads/NonProd"
         ]
-        core_accounts : {
-          org_management : ""
-          security : ""
-          connectivity : ""
-          log_archive : ""
+        core_account_ids : {
+          org_management : "123456789000"
+          security : "123456789000"
+          connectivity : "123456789000"
+          log_archive : "123456789000"
+        }
+        core_account_tags : {
+          org_management : jsonencode({tag1: "value", tag2: "value"})
+          security : jsonencode({})
+          connectivity : jsonencode({})
+          log_archive : jsonencode({})
         }
       }
     }
@@ -36,6 +45,57 @@ locals {
     connectivity : {
       route53_public_hosted_zones : [
         "example.com"
+      ]
+    }
+  }
+
+  # to avoid additional levels inside the paramter store, the keys itself two keys can be combines
+  foundation_parameters_2 = {
+    "management.organization" : {
+      units : [
+          "Suspended",
+          "Exceptions",
+          "Infrastructure",
+          "Security",
+          "Workloads/Prod",
+          "Workloads/NonProd"
+        ]
+        core_account_ids : {
+          org_management : "123456789000"
+          security : "123456789000"
+          connectivity : "123456789000"
+          log_archive : "123456789000"
+        }
+        core_account_tags : {
+          org_management : jsonencode({tag1: "value", tag2: "value"})
+          security : jsonencode({})
+          connectivity : jsonencode({})
+          log_archive : jsonencode({})
+        }
+    }
+    "management.region" : {
+      main_region : "eu-central-1"
+      active_regions : ["eu-central-1", "eu-central-2"]
+    }
+    "management.iam" : {
+      read_tags_role_name : "read-account-tags-role"
+      account_access_role : "OrganizationAccountAccessRole"
+    }
+    "security.config" : {}
+    "security.delegation" : {
+      securityhub : true
+      guardduty : false
+      config : true
+      firewall_manager : true
+    }
+    "connectivity.route53" : {
+      public_hosted_zones : [
+        "example.com"
+      ]
+    }
+    "connectivity.ipam" : {
+      cidr : [
+        "10.0.0.0/8"
       ]
     }
   }
